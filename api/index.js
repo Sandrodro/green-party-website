4c52ec57391aacedd35d342f6c154d59c55ee98e
Sandrodro <sandrogach@gmail.com>
2022-01-20 20:02:57 +0400

    cleaning up

--- api/index.js
+++ api/index.js
@@ -3,21 +3,18 @@ import ROUTES from "../config/API_ROUTES";
 async function getAllData() {
   const data = await fetch(ROUTES.ALL_DATA);
   const json = await data.json();
-  console.log("DATA", json);
   return json;
 }
 
 async function getNewsArticles() {
   const newsArticles = await fetch(ROUTES.NEWS_PAGES);
   const json = await newsArticles.json();
-  console.log("Posts", json);
   return json;
 }
 
 async function getSingleNewsArticle(id) {
   const newsArticle = await fetch(`${ROUTES.SINGLE_NEWS_PAGE}${id}`);
   const json = await newsArticle.json();
-  console.log("POST", json);
   return json;
 }
 
